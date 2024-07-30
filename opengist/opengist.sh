18e47a6e36795addfe4c475df655831251c24450
schBenedikt <benedikt.schaechner@web.de>
2024-07-30 15:49:12 +0200

    Update opengist.sh

--- opengist/opengist.sh
+++ opengist/opengist.sh
@@ -4,14 +4,23 @@ ORDNERNAME="opengist"
 
 USERNAME=$USER
 
-mkdir -p /home/$USERNAME/opengist
+mkdir -p /home/$USERNAME/$ORDNERNAME
 
-cd /home/$USERNAME/opengist
+cd /home/$USERNAME/$ORDNERNAME
 
 wget https://raw.githubusercontent.com/SchBenedikt/easy-docker/main/opengist/docker-compose.yml
 
 if [ $? -eq 0 ]; then
-    echo "Die Datei wurde erfolgreich heruntergeladen und befindet sich in /home/$USERNAME/opengist/docker-compose.yml"
+    echo "Die Datei wurde erfolgreich heruntergeladen und befindet sich in /home/$USERNAME/$ORDNERNAME/docker-compose.yml"
+    
+    # Ausführen von 'sudo docker compose up -d'
+    sudo docker compose up -d
+    
+    if [ $? -eq 0 ]; then
+        echo "'sudo docker compose up -d' wurde erfolgreich ausgeführt."
+    else
+        echo "Beim Ausführen von 'sudo docker compose up -d' ist ein Fehler aufgetreten."
+    fi
 else
     echo "Beim Herunterladen der Datei ist ein Fehler aufgetreten."
 fi
