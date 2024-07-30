65729c1cf0e126b10e77ac0d8cc1c8add461859e
schBenedikt <benedikt.schaechner@web.de>
2024-07-30 15:35:23 +0200

    Update opengist.sh

--- opengist/opengist.sh
+++ opengist/opengist.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
-# Definieren von Benutzernamen und Ordnernamen
-USERNAME="benedikt"
-ORDNERNAME="ORDNERNAME"
+ORDNERNAME="opengist"
 
-# Erstellen des Ordners
+USERNAME=$USER
+
 mkdir -p /home/$USERNAME/$ORDNERNAME
 
-# Wechseln in den erstellten Ordner
 cd /home/$USERNAME/$ORDNERNAME
 
-# Herunterladen der Datei docker-compose.yml
 wget https://raw.githubusercontent.com/SchBenedikt/easy-docker/main/opengist/docker-compose.yml
 
-# Bestätigung der erfolgreichen Ausführung
 if [ $? -eq 0 ]; then
     echo "Die Datei wurde erfolgreich heruntergeladen und befindet sich in /home/$USERNAME/$ORDNERNAME/docker-compose.yml"
 else
