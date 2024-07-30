207d2fcc081f20fff01fb0fce9be8b33e2ff67b5
schBenedikt <benedikt.schaechner@web.de>
2024-07-30 15:31:16 +0200

    Update opengist.sh

--- opengist/opengist.sh
+++ opengist/opengist.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-download.sh
+# URL der download.sh Datei
+DOWNLOAD_SCRIPT_URL="https://raw.githubusercontent.com/SchBenedikt/easy-docker/main/opengist/download.sh"
 
+# Datei lokal speichern
+curl -O $DOWNLOAD_SCRIPT_URL
+
+# Datei ausführbar machen
+chmod +x download.sh
+
+# download.sh ausführen
+./download.sh
+
+# Prüfen, ob download.sh erfolgreich ausgeführt wurde
 if [ $? -eq 0 ]; then
+  # Ausführen von "sudo docker-compose up -d"
   sudo docker-compose up -d
 else
-  echo "download_script.sh fehlgeschlagen. Docker Compose wird nicht gestartet."
+  echo "download.sh fehlgeschlagen. Docker Compose wird nicht gestartet."
 fi
